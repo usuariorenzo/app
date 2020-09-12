2cd434925d304250c7473e797174ce7707df11d6
usuario.renzo <usuario.renzo@gmail.com>
2020-09-11 22:56:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function smoothScrollingTo(target){
+     $('html,body').animate({scrollTop:$(target).offset().‌​top}, 500);
+}
+$('a[href*=\\#]').on('click', function(event){     
+       event.preventDefault();
+       smoothScrollingTo(this.hash);
+});
+$(document).ready(function(){
+     smoothScrollingTo(location.hash);
+});
